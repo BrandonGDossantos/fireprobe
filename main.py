f7f2a7aeb307e098c167b074f062413318be3cdc
BrandonGDossantos <bxd7887@g.rit.edu>
2018-04-03 20:45:33 -0400

    Init main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+import iptc
+
+table = iptc.Table(iptc.Table.FILTER)
+print(table.name)
+# for chain in table.chains:
+#     print("=======================")
+#     print("Chain ", chain.name)
+#     for rule in chain.rules:
+#         print("Rule","proto:",rule.protocol,"src:",rule.src,"dst:",rule.dst,"in:",rule.in_interface,"out:",rule.out_interface)
+#         print("Matches:"),
+#         for match in rule.matches:
+#             print(match.name),
+#             print("Target:"),
+#             print(rule.target.name)
+# print("=======================")
+#
+
