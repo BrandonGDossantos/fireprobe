04f3d5dd4862ae91b5ff40ae323b0fc793430af4
BrandonGDossantos <bxd7887@rit.edu>
2018-04-04 20:00:31 -0700

    Printed dictionary

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ class ruleTuple(object):
 		self.protocol = protocol
 		self.single_src = single_src
 		self.single_dst = single_dst
+		self.range_src = range_src
+		self.range_dst = range_dst
 		self.sport = sport
 		self.dport = dport
 		self.action = action
@@ -15,9 +17,7 @@ def main():
 	for chain in table.chains:
 		for rule in chain.rules:
 			print("======================RULE {0} IN CHAIN=============================".format(x))
-			# protocol = rule.protocol
-			# single_src = rule.src
-			# single_dst = rule.dst
+			print("\n")
 			x+=1
 			for match in rule.matches:
 				print("----------------------------{}------------------------------".format("Match"))
@@ -30,25 +30,10 @@ def main():
 				# print("[Range Source Address] Match src_range: {}".format(match.src_range))
 				# print("[Range Destination Address] Match dst_range: {}".format(match.dst_range))
 				# print("[Action] Rule Target Name: {}".format(rule.target.name))
-
-				if rule.protocol:
-					print("Protocol", str(rule.protocol))
-				if rule.src != "0.0.0.0/0.0.0.0":
-					print("Src", str(rule.src))
-				if rule.dst != "0.0.0.0/0.0.0.0":
-					print("Dst",str(rule.dst))
-				if match.dport:
-					print("Dport", str(match.dport))
-				if match.sport:
-					print("Sport", str(match.sport))
-				if match.src_range:
-					print("Src_range", str(match.src_range))
-				if match.dst_range:
-					print("Dst_range", str(match.dst_range))
-				if rule.target.name:
-					print("Action", str(rule.target.name))		
-
-
+				r = ruleTuple(str(rule.protocol), str(rule.src), str(rule.dst), str(match.src_range), str(match.dst_range), str(match.sport), str(match.dport), str(rule.target.name)) 
+				print(r.__dict__)
+				print("\n")
+				
 if __name__ == "__main__":
 	main()
 
--- tableSetup.sh
+++ tableSetup.sh
@@ -2,6 +2,10 @@
 iptables -F
 iptables -X
 
+iptables -A OUTPUT -p udp --sport 88 -m iprange --dst-range 44.44.0.3-44.44.10.3 -j DROP
+iptables -A INPUT -p udp --dport 34:45 -s 19.0.0.0/8 -j ACCEPT
+iptables -A INPUT -p tcp --dport 22 -s 122.33.22.0/24 -j DROP
+iptables -A OUTPUT -p tcp --dport 33 -s 10.50.0.0/16 -j ACCEPT
 iptables -A OUTPUT -p tcp --sport 53566 -d 222.222.222.222 -j ACCEPT 
 iptables -A INPUT -p tcp --dport 23 -s 1.2.3.4 -d 129.192.129.192 -j DROP 
 iptables -A INPUT -p tcp --dport 100:110 -m iprange --src-range 192.188.229.0-192.255.255.255 -j DROP
