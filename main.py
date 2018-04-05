3e5544e739b150fe13795b4916e297b14ad66a2a
BrandonGDossantos <bxd7887@rit.edu>
2018-04-04 21:00:16 -0700

    Playin around

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 import iptc
 
 class ruleTuple(object):
-	def __init__(self, protocol, single_src, single_dst, range_src, range_dst, sport, dport, action):
+	def __init__(self, protocol, src, dst, sport, dport, action):
 		self.protocol = protocol
-		self.single_src = single_src
-		self.single_dst = single_dst
-		self.range_src = range_src
-		self.range_dst = range_dst
+		self.src = src
+		self.dst = dst
 		self.sport = sport
 		self.dport = dport
 		self.action = action
@@ -18,18 +16,20 @@ def printTupList(ruleTuple):
 	#print(ruleTuple.tupList)
 	r = {k:v for k, v in vars(ruleTuple).items() if v != 'None'}
 	print(r)
-	z = {**	
 	#print ', '.join("%s: %s" % item for item in r.items())
 def extract(table):
 	x = 1	
 	for chain in table.chains:
 		for rule in chain.rules:
+			l = set()
 			print("======================RULE {0} IN CHAIN=============================".format(x))
 			print("\n")
 			x+=1
+			print(l)
 			for match in rule.matches:
 				print("----------------------------{}------------------------------".format("Match"))
-				r = ruleTuple(str(rule.protocol), str(rule.src), str(rule.dst), str(match.src_range), str(match.dst_range), str(match.sport), str(match.dport), str(rule.target.name)) 
+				l.(str(rule.protocol), str(match.src_range), str(match.dst_range), str(match.sport), str(match.dport), str(rule.target.name))
+				r = ruleTuple(str(rule.protocol), str(match.src_range), str(match.dst_range), str(match.sport), str(match.dport), str(rule.target.name)) 
 				#print(r.__dict__)
 				#r.tupList.append(r.__dict__)	
 				printTupList(r)
--- tableSetup.sh
+++ tableSetup.sh
@@ -3,7 +3,7 @@ iptables -F
 iptables -X
 
 iptables -A OUTPUT -p udp --sport 88 -m iprange --dst-range 44.44.0.3-44.44.10.3 -j DROP
-iptables -A INPUT -p udp --dport 34:45 -s 19.0.0.0/8 -j ACCEPT
+iptables -A INPUT -p udp --dport 34:45 -m iprange --src-range 19.0.0.2 -j ACCEPT
 iptables -A INPUT -p tcp --dport 22 -s 122.33.22.0/24 -j DROP
 iptables -A OUTPUT -p tcp --dport 33 -s 10.50.0.0/16 -j ACCEPT
 iptables -A OUTPUT -p tcp --sport 53566 -d 222.222.222.222 -j ACCEPT 
