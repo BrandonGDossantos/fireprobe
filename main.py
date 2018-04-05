358972f8fe62e67fc76e278bd4da027f793c5a59
BrandonGDossantos <bxd7887@rit.edu>
2018-04-04 17:09:11 -0700

    Print values

--- main.py
+++ main.py
@@ -1,22 +1,60 @@
 import iptc
 
-table = iptc.Table(iptc.Table.FILTER)
-for chain in table.chains:
+class ruleTuple(object):
+	def __init__(self, protocol, single_src, single_dst, range_src, range_dst, sport, dport, action):
+		self.protocol = protocol
+		self.single_src = single_src
+		self.single_dst = single_dst
+		self.sport = sport
+		self.dport = dport
+		self.action = action
+
+def main():	
+	table = iptc.Table(iptc.Table.FILTER)
 	x = 1	
-	for rule in chain.rules:
-		print("======================RULE {} IN CHAIN=============================".format(x))
-		print("[]Proto: {}".format(rule.protocol))
-		print("[]Src: {}".format(rule.src))
-		print("[]Dst: {}".format(rule.dst))
-		print("In Interface: {}".format(rule.in_interface))
-		print("Out Interface: {}".format(rule.out_interface))
-		x+=1
-		for match in rule.matches:
-			print("\t\t\t======================MATCH IN RULE=============================")
-			print("\t\t\t[]Dst port: {}".format(match.dport))
-			print("\t\t\t[]Src port: {}".format(match.srcport))
-			print("\t\t\tMatch src_range: {}".format(match.src_range))
-			print("\t\t\tMatch dst_range: {}".format(match.dst_range))
-			print("\t\t\tMatch name: {}".format(match.name))
-			print("\t\t\tRule Target Name: {}".format(rule.target.name))
+	for chain in table.chains:
+		for rule in chain.rules:
+			print("======================RULE {0} IN CHAIN=============================".format(x))
+			# protocol = rule.protocol
+			# single_src = rule.src
+			# single_dst = rule.dst
+			x+=1
+			for match in rule.matches:
+				print("----------------------------{}------------------------------".format("Match"))
+				# print("[Protocol] Proto: {}".format(rule.protocol))
+				# print("[Single Source Address] Src: {}".format(rule.src))
+				# print("[Single Destination Address] Dst: {}".format(rule.dst))
+				# print("Chain Name: {}".format(chain.name))
+				# print("[Destination Port] Dst port: {}".format(match.dport))
+				# print("[Source Port] Src port: {}".format(match.sport))
+				# print("[Range Source Address] Match src_range: {}".format(match.src_range))
+				# print("[Range Destination Address] Match dst_range: {}".format(match.dst_range))
+				# print("[Action] Rule Target Name: {}".format(rule.target.name))
+
+				if rule.protocol:
+					print("Protocol", str(rule.protocol))
+				if rule.src != "0.0.0.0/0.0.0.0":
+					print("Src", str(rule.src))
+				if rule.dst != "0.0.0.0/0.0.0.0":
+					print("Dst",str(rule.dst))
+				if match.dport:
+					print("Dport", str(match.dport))
+				if match.sport:
+					print("Sport", str(match.sport))
+				if match.src_range:
+					print("Src_range", str(match.src_range))
+				if match.dst_range:
+					print("Dst_range", str(match.dst_range))
+				if rule.target.name:
+					print("Action", str(rule.target.name))		
+
+
+if __name__ == "__main__":
+	main()
+
+
+
+
+
+
 
--- tableSetup.sh
+++ tableSetup.sh
@@ -2,8 +2,9 @@
 iptables -F
 iptables -X
 
-iptables -A OUTPUT -p tcp --sport 22 -s 1.2.3.4 -d 129.192.129.192 -j DROP 
-iptables -A INPUT -p tcp --dport 22 -s 1.2.3.4 -d 129.192.129.192 -j DROP 
+iptables -A OUTPUT -p tcp --sport 53566 -d 222.222.222.222 -j ACCEPT 
+iptables -A INPUT -p tcp --dport 23 -s 1.2.3.4 -d 129.192.129.192 -j DROP 
 iptables -A INPUT -p tcp --dport 100:110 -m iprange --src-range 192.188.229.0-192.255.255.255 -j DROP
-iptables -A INPUT -p tcp --destination-port 100:110 -m iprange --src-range 142.192.0.0-142.192.255.255 -j ACCEPT
-iptables -A INPUT -p tcp --dport 100:110 -m iprange --src-range 192.188.229.0-192.255.255.255 --dst-range 122.222.222.0-122.222.222.255 -j DROP
+iptables -A INPUT -p tcp --destination-port 100:130 -m iprange --src-range 142.192.0.0-142.192.255.255 -j ACCEPT
+iptables -A INPUT -p tcp --dport 7777 -m iprange --src-range 192.188.229.0-192.255.255.255 --dst-range 122.222.222.0-122.222.222.255 -j DROP
+iptables -A OUTPUT -p tcp --sport 22 -s 1.2.3.4 -d 129.192.129.192 -j DROP 
