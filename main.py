144821cee6bd0cb4d5dc6feeb78a38ed1f4eecc6
BrandonGDossantos <bxd7887@rit.edu>
2018-04-07 14:20:43 -0700

    Got tuples

--- main.py
+++ main.py
@@ -2,131 +2,97 @@ import iptc
 from netaddr import IPAddress
 import ipaddress
 
-class tupleList(object):
-	def __init__(self):
-		self.list = []
-		self.tup = ()
-class ruleTuple(object):
-	def __init__(self):
-		self.list = []
-		self.tup = ()
-	def fill(self, protocol, src, dst, src_range, dst_range, sport, dport, action):
-		self.protocol = protocol
-		self.src_range = src_range
-		self.dst_range = dst_range
-		self.src = src
-		self.dst = dst
-		self.sport = sport
-		self.dport = dport
-		self.action = action
+PROTO_MAP = {'tcp':6, 'udp':17}
+SRC_MAP = {'0.0.0.0/0.0.0.0': ('0.0.0.0', '255.255.255.255'), 'None':('0.0.0.0', '255.255.255.255')}
+PRT_MAP = {'None':(0, 65535)}
+ACTION_MAP = {'ACCEPT': 0, 'DROP':1}
 
-def clean(t):
-	for obj in t.list:
-		obj.list = []
-		# dictionary for this
-		if obj.protocol == 'tcp':
-			obj.protocol = (6,6)
-			obj.list.append(obj.protocol)
-		elif obj.protocol == 'udp':
-			obj.protocol = (17,17)
-			obj.list.append(obj.protocol)
-		if obj.src_range == 'None':
-			obj.src_range = ('0.0.0.0','255.255.255.255')
-			obj.list.append(obj.src_range)
+RULE_OBJ_LIST = []
+
+class Rule(object):
+	def set_protocol(self, protocol):
+			self.protocol = (PROTO_MAP[protocol], PROTO_MAP[protocol])
+	def set_pairs(self, src, srcRange, sport, dst, dstRange, dport):
+		if srcRange == 'None' and dstRange == 'None':
+				self.src = clean_ip(src)
+				self.dst = clean_ip(dst)
+				self.sport = clean_port(sport)
+				self.dport = clean_port(dport)
 		else:
-			obj.src_range = tuple(obj.src_range.split('-'))
-			obj.list.append(obj.src_range)
-		if obj.dst_range == 'None':
-			obj.dst_range = ('0.0.0.0','255.255.255.255')
-			obj.list.append(obj.dst_range)
-		else:
-			obj.dst_range = tuple(obj.dst_range.split('-'))
-			obj.list.append(obj.dst_range)
-		if obj.src == '0.0.0.0/0.0.0.0':
-			obj.src = ('0.0.0.0','255.255.255.255')
-			obj.list.append(obj.src)
-		else:
-			ip, netmask = obj.src.split('/')
-			n = ipaddress.IPv4Network(u'{}'.format("{}/{}".format(ip, IPAddress(netmask).netmask_bits())))
-			obj.src = tuple([str(n[0]), str(n[-1])])
-			obj.list.append(obj.src)
-		if obj.dst == '0.0.0.0/0.0.0.0':
-			obj.dst = ('0.0.0.0','255.255.255.255')
-			obj.list.append(obj.dst)
-		else:
-			ip, netmask = obj.src.split('/')
-			n = ipaddress.IPv4Network(u'{}'.format("{}/{}".format(ip, IPAddress(netmask).netmask_bits())))
-			obj.src = tuple([str(n[0]), str(n[-1])])
-			obj.list.append(obj.src)
-		if obj.sport == 'None':
-			obj.sport = (0,65535)
-			obj.list.append(obj.sport)
-		else:
-			b = obj.dport.split(':')
-			if len(b) == 1:
-				b.append(b[0])
-			obj.dport = tuple(b)
-		if obj.dport == 'None':
-			obj.dport = (0,65535)
-			obj.list.append(obj.dport)
-		else:
-			print(obj.dport)
-			b = obj.dport.split(':')
-			if len(b) == 1:
-				b.append(b[0])
-			obj.dport = tuple(b)
-			obj.list.append(obj.dport)
-		if obj.action == 'ACCEPT':
-			obj.action = 0
-			obj.list.append(obj.action)
-		elif obj.action == 'DROP':
-			obj.action = 1 
-			obj.list.append(obj.action)
-		obj.tup = tuple(obj.list)
+			self.src = clean_ip_range(srcRange)
+			self.dst = clean_ip_range(dstRange)
+	def set_action(self, action):
+		self.action = ACTION_MAP[action]
+
+def get_tuple(obj):
+	values = []
+	for item in vars(obj).items():
+		values.append(item)
+	return tuple(values)
+
+def clean_ip(ip):
+	try:
+		return SRC_MAP[ip]
+	except KeyError:
+		ipaddr, netmask = ip.split('/')
+		split_ip = ipaddress.IPv4Network(u'{}'.format("{}/{}".format(ipaddr, IPAddress(netmask).netmask_bits())))
+		return tuple([str(split_ip[0]), str(split_ip[-1])])
 	
-def merge_two_dicts(t):
-	for obj in t.list:
-		if len(obj.list) == 2:
-			x = obj.list[0]
-			y = obj.list[1]
-			z = dict(x.items()+y.items())
-			del z['list']
-			obj.list = z
-		else:
-			del obj.list[0]['list'] 
-			obj.list = obj.list[0]	
-	return t
+def clean_ip_range(ip):
+	try:
+		return SRC_MAP[ip]
+	except KeyError:
+		split_ip = ip.split('-')
+		return tuple([str(split_ip[0]), str(split_ip[1])])
+def clean_port(port):
+	try:
+		return PRT_MAP[port]
+	except KeyError:
+		split_port = [int(x) for x in port.split(':')]
+		if len(split_port) == 1:
+			split_port.append(int(split_port[0]))
+		return tuple(split_port)
+
+		
+def print_rule_objects(obj_list):
+	print(obj_list)
+	#for obj in obj_list:
+
+def print_orig(rule, match, y):
+	print("**************Original-{}********************".format(y))
+	print("Protocol: {}".format(rule.protocol))
+	print("Src: {}".format(rule.src))
+	print("Dst: {}".format(rule.dst))
+	print("Src Range: {}".format(match.src_range))
+	print("Dst Range: {}".format(match.dst_range))
+	print("Sport: {}".format(match.sport))
+	print("Dport: {}".format(match.dport))
+	print("Action: {}".format(rule.target.name))
+
 
 def printTupList(t):
 	for obj in t.list:
 		print(obj.tup)
 		print("\n")
+
 def extract(table):
-	objList = tupleList()
+#	objList = TupleList()
 	for chain in table.chains:
 		for rule in chain.rules:
-			r = ruleTuple()
+			rule_obj = Rule()
 			y = 0
 			for match in rule.matches:
-				y+=1 
-				r.fill(str(rule.protocol), str(rule.src), str(rule.dst), str(match.src_range), str(match.dst_range), str(match.sport), str(match.dport), str(rule.target.name)) 
-				#noNoneDict = {k:v for k, v in vars(r).items() if v != 'None'}
-
-				noNoneDict = {k:v for k, v in vars(r).items()}
-				r.list.append(noNoneDict)
+				y+=1
+				rule_obj.set_protocol(str(rule.protocol))		
+				rule_obj.set_pairs(str(rule.src), str(match.src_range), str(match.sport), str(rule.dst), str(match.dst_range), str(match.dport))
+				rule_obj.set_action(str(rule.target.name))
 				if y == len(rule.matches):
-					objList.list.append(r)		
-				# print("\n")
-	clean(merge_two_dicts(objList))	
-	printTupList(objList)			
-
+					RULE_OBJ_LIST.append(get_tuple(rule_obj))
+					# objList.list.append(rule_obj)	
 
 def main():	
-	n = ipaddress.IPv4Network(u'10.10.128.0/24')
-	first, last = n[0], n[-1]
-	# print(first, last)
-	extract(iptc.Table(iptc.Table.FILTER))
-
+	objList = extract(iptc.Table(iptc.Table.FILTER))
+	print_rule_objects(RULE_OBJ_LIST)
 if __name__ == "__main__":
 	main()
 
